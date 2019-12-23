e1830d9a8cf6deccc984777c91fe51a5137cf560
Roman Cherepanov <romach007@gmail.com>
2019-12-23 21:21:41 +0200

    Complete first task

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,27 @@
 // Your code goes here
+'use strict'
+const a = prompt('Input a: ')
+const b = prompt('Input b: ')
+const c = prompt('Input c: ')
+if (
+  isNaN(isFinite(a) && a !== null && a.trim().length > 0 ? Number(a) : NaN) ||
+  a === 0 ||
+  isNaN(isFinite(a) && a !== null && a.trim().length > 0 ? Number(a) : NaN) ||
+  isNaN(isFinite(a) && a !== null && a.trim().length > 0 ? Number(a) : NaN)
+) {
+  console.log('Invalid input data')
+} else {
+  const FOUR = 4
+  const TWO = 2
+  const discriminant = b * b - FOUR * (a * c)
+  if (discriminant === 0) {
+    const singleAnswer = Math.round(-(b / (TWO * a)))
+    console.log(`x1 = x2 = ${singleAnswer}`)
+  } else if (discriminant > 0) {
+    const firstAnswer = Math.round((-b + Math.sqrt(discriminant)) / (TWO * a))
+    const secondAnswer = Math.round((-b - Math.sqrt(discriminant)) / (TWO * a))
+    console.log(`x1 = ${firstAnswer} and x2 = ${secondAnswer}`)
+  } else {
+    console.log('no solution')
+  }
+}
