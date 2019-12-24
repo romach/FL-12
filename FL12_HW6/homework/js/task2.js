7dc8e3292c4b62982ef9f845fbf9cbd893f00707
Roman Cherepanov <romach007@gmail.com>
2019-12-24 16:42:41 +0200

    Fix logic in second task

--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -12,22 +12,23 @@ if (
   cInput === undefined
 ) {
   alert('input values should be ONLY numbers')
-}
-const a = parseInt(aInput)
-const b = parseInt(bInput)
-const c = parseInt(cInput)
-
-if (a === 0 || b === 0 || c === 0) {
-  alert('A triangle must have 3 sides with a positive definite length')
-} else if (a + b <= c || a + c <= b || b + c <= a) {
-  alert("Triangle doesn't exist")
-  console.log("Triangle doesn't exist")
 } else {
-  if (a === b && b === c) {
-    console.log('Equilateral triangle')
-  } else if (a === b || a === c || b === c) {
-    console.log('Isosceles triangle')
+  const a = parseInt(aInput)
+  const b = parseInt(bInput)
+  const c = parseInt(cInput)
+
+  if (a === 0 || b === 0 || c === 0) {
+    alert('A triangle must have 3 sides with a positive definite length')
+  } else if (a + b <= c || a + c <= b || b + c <= a) {
+    alert("Triangle doesn't exist")
+    console.log("Triangle doesn't exist")
   } else {
-    console.log('Scalene triangle')
+    if (a === b && b === c) {
+      console.log('Equilateral triangle')
+    } else if (a === b || a === c || b === c) {
+      console.log('Isosceles triangle')
+    } else {
+      console.log('Scalene triangle')
+    }
   }
 }
