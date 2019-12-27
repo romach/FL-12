55674595e8e82a80362718f8aeb7b3ebb6281f6b
Roman Cherepanov <romach007@gmail.com>
2019-12-28 00:57:46 +0200

    Meke second task of HW7

--- FL12_HW7/homework/src/js/task1.js
+++ FL12_HW7/homework/src/js/task1.js
@@ -1,73 +1,56 @@
 // Your code goes here
-
-// Task #1. Check the email and change the password
-// Write the code which verify user rights.
-
-// Step 1. Check login
-//     • Ask user for an email // use prompt()
-const MINIMUM_EMAIL_LENGTH = 5
-const USER_EMAIL = 'user@gmail.com'
-const USER_PASSWORD = 'UserPass'
-const ADMIN_EMAIL = 'admin@gmail.com'
-const ADMIN_PASSWORD = 'AdminPass'
-const NEW_PASSWORD_MINIMUM_LENGTH = 6
-
-const email = prompt('Enter your e-mail')
-//     • If the input is an empty line or Esc – show “Canceled.” // for showing - use alert()
+const MINIMUM_EMAIL_LENGTH = 5;
+const USER_EMAIL = 'user@gmail.com';
+const USER_PASSWORD = 'UserPass';
+const ADMIN_EMAIL = 'admin@gmail.com';
+const ADMIN_PASSWORD = 'AdminPass';
+const NEW_PASSWORD_MINIMUM_LENGTH = 6;
+const email = prompt('Enter your e-mail');
 if (email === null || email === '') {
-  alert('Canceled')
-//     • If the input length less than 5 symbols - show “I don't know any emails having name length less than 5 symbols”. 
+  alert('Canceled');
 } else if (email.length < MINIMUM_EMAIL_LENGTH) {
-  alert('I don\'t know any emails having name length less than 5 symbols')
-//     • If the visitor enters "user@gmail.com" or "admin@gmail.com" then prompt for a password.
+  alert('I don\'t know any emails having name length less than 5 symbols');
 } else if (email === USER_EMAIL || email === ADMIN_EMAIL) {
-  // Step 2. Check password:
-  const password = prompt('Enter password')
-  // • For an empty string or cancelled input, show “Canceled.”
+  const password = prompt('Enter password');
   if (password === null || password === '') {
-    alert('Canceled')
-  // • For email "user@gmail.com" correct password is “UserPass”, for "admin@gmail.com" correct password is  “AdminPass”.
-  } else if ((email === USER_EMAIL && password === USER_PASSWORD) || (email === ADMIN_EMAIL && password === ADMIN_PASSWORD)) {
-    // Step 3. Change the password:
-    //     • 1) Suggest user/admin to change his password – “Do you want to change your password?”. //use confirm()
-    const changePasswordConfirmation = confirm('Do you want to change your password?')
-    //     • In case the user clicks the 'Cancel' button, the message “You have failed the change.” //use alert()
+    alert('Canceled');
+  } else if (
+    email === USER_EMAIL && password === USER_PASSWORD ||
+    email === ADMIN_EMAIL && password === ADMIN_PASSWORD
+  ) {
+    const changePasswordConfirmation = confirm(
+      'Do you want to change your password?'
+    );
     if (!changePasswordConfirmation) {
-      alert('You have failed the change')
+      alert('You have failed the change');
     } else {
-      //     • 2) If user clicked ‘Ok’ – ask to write the old password (use prompt() ) and validate it as at Step 2. 
-      const oldPassword = prompt('Enter old password')
+      const oldPassword = prompt('Enter old password');
       if (oldPassword === null || oldPassword === '') {
-        alert('Canceled')
-      } else if ((email === USER_EMAIL && oldPassword === USER_PASSWORD) || (email === ADMIN_EMAIL && oldPassword === ADMIN_PASSWORD)) {
-        //     • 3) If the visitor enters correct old password for current email then prompt for a new password.
-        const newPassword = prompt('Enter new password')
-        //     • If the input is an empty line or ‘Cancel’ button is clicked  – show “Canceled” //use alert()
+        alert('Canceled');
+      } else if (
+        email === USER_EMAIL && oldPassword === USER_PASSWORD ||
+        email === ADMIN_EMAIL && oldPassword === ADMIN_PASSWORD
+      ) {
+        const newPassword = prompt('Enter new password');
         if (newPassword === null || newPassword === '') {
-          alert('Canceled')
-          //     • If the input length less than 6 – show  “It’s too short password. Sorry.”
+          alert('Canceled');
         } else if (newPassword.length < NEW_PASSWORD_MINIMUM_LENGTH) {
-          alert('It’s too short password. Sorry.')
-          //     • 4) If the new password is valid ask to enter it again.//use prompt()
+          alert('It’s too short password. Sorry.');
         } else {
-          const newPasswordRepeat = prompt('Enter new password again')
-          //     • If the inputted value  doesn’t match the new password from 3) – show “You wrote the wrong password.”
+          const newPasswordRepeat = prompt('Enter new password again');
           if (newPasswordRepeat !== newPassword) {
-            alert('You wrote the wrong password.')
-            //     • If user write the same new – show “You have successfully changed your password.” //use alert()
+            alert('You wrote the wrong password.');
           } else {
-            alert('You have successfully changed your password.')
+            alert('You have successfully changed your password.');
           }
         }
       } else {
-        alert('Wrong old password')
+        alert('Wrong old password');
       }
     }
   } else {
-    //   In other case, show “Wrong password”.
-    alert('Wrong password')
+    alert('Wrong password');
   }
 } else {
-  //     • If it’s another string – then show “I don’t know you”.
-  alert('I don\'t know you')
+  alert("I don't know you");
 }
--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,28 +1,65 @@
 // Your code goes here
-// Task #2. Guessing game
-// Your task is to write a simple simulator of casino roulette. 
-
-// Requirements:
-// Step 1:
-//     • Create a prompt window (use confirm()). Show the message inside the window ‘Do you want to play a game?’.
-const playGameConfirmation = confirm('Do you want to play a game?')
+const playGameConfirmation = confirm('Do you want to play a game?');
 if (!playGameConfirmation) {
-  //     • In case the user clicks the 'Cancel' button, the message 'You did not become a billionaire, but can.' should be shown (use alert).
-  alert('You did not become a billionaire, but can.')
+  alert('You did not become a billionaire, but can.');
 } else {
-  // Step 2:
-  //• If user clicked ‘Ok’ – start a game: randomly (use Math.random()) choose an integer number in range [0; 8] (including 0 and 8)
-  // and ask user to enter a number of pocket on which the ball could land (use prompt()).
-
+  const ONE = 1;
+  const INITIAL_MAXIMUM_GUESS_RANGE_SIZE = 8;
+  let maximumGuessRangeSize = INITIAL_MAXIMUM_GUESS_RANGE_SIZE;
+  let prizes = {
+    1: 100,
+    2: 50,
+    3: 25
+  };
+  let gameIsOn = true;
+  let totalUserPrize = 0;
+  while (gameIsOn) {
+    const GUESS_ATTEMPTS_COUNT = 3;
+    const winNumber = Math.floor(Math.random() * (maximumGuessRangeSize + ONE));
+    let currentAttemptNumber = 1;
+    let userHasGuessed = false;
+    while (currentAttemptNumber <= GUESS_ATTEMPTS_COUNT && !userHasGuessed) {
+      const userGuess = parseInt(
+        prompt(`Choose a roulette pocket number from 0 to ${maximumGuessRangeSize}
+Attempts left: ${GUESS_ATTEMPTS_COUNT - currentAttemptNumber + 1}
+Total prize: ${totalUserPrize}$
+Possible prize on current attempt: ${prizes[currentAttemptNumber]}$`)
+      );
+      if (userGuess === winNumber) {
+        userHasGuessed = true;
+      }
+      currentAttemptNumber++;
+    }
+    if (!userHasGuessed) {
+      alert(
+        `Thank you for your participation. Your prize is: ${totalUserPrize}`
+      );
+      gameIsOn = confirm('Do you want to play again?');
+      prizes = {
+        1: 100,
+        2: 50,
+        3: 25
+      };
+      maximumGuessRangeSize = INITIAL_MAXIMUM_GUESS_RANGE_SIZE;
+      totalUserPrize = 0;
+    } else {
+      totalUserPrize += prizes[currentAttemptNumber];
+      const continueConfirmation = confirm(`Congratulation, you won!
+Your prize is: ${totalUserPrize} $.
+Do you want to continue?`);
+      if (!continueConfirmation) {
+        alert(
+          `Thank you for your participation. Your prize is: ${totalUserPrize} $`
+        );
+        gameIsOn = confirm('Do you want to play again?');
+      } else {
+        const TWO = 2;
+        const NUMBER_RANGE_ENLARGEMENT = 4;
+        maximumGuessRangeSize += NUMBER_RANGE_ENLARGEMENT;
+        for (const [place, sum] of Object.entries(prizes)) {
+          prizes[place] = sum * TWO;
+        }
+      }
+    }
+  }
 }
-//     • User has 3 attempts to guess a number.
-//     • If user guessed the number on which ball landed, on 1-st attempt prize is 100$ (maximum prize for current numbers range), 2-nd attempt – 50$, 3-rd attempt – 25$. 
-//     • If user did not guess a number show the message ‘Thank you for your participation. Your prize is: … $’ (Use alert) and ask if he wants to play again (use confirm).
-// Step 3:
-//     • If user did guess - Show the message ‘Congratulation, you won!   Your prize is: … $. Do you want to continue?’.
-//     • If user does not want to continue – show the message ‘Thank you for your participation. Your prize is: … $’ (Use alert) and ask if he wants to play again (use confirm).
-//     • If user does want to continue, make number range bigger at 4 as the previous one (for example [0; 8] -> [0; 12]), and two times bigger maximum prize (for example on 1-st attempt prize will be 200$, 2-nd attempt – 100$, 3-rd attempt – 50$). Prize must be added to the previous one and number of attempts should be set to 3 (user should have 3 attempts to guess a number for each numbers range)
-//     • Each time you ask user to enter a number you should show him a range of cells, how much attempts he has left, his total prize and possible prize on current attempt. See Figure 1:
-
-
-//     • All these stuffs should be repeated until user lose or decide to quit
