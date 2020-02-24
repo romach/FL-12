193e9537cacdccf34e377a94ae18a8e5de1049d4
Roman Cherepanov <romach007@gmail.com>
2020-02-24 23:13:38 +0200

    remove usage examples

--- FL12_Webinar_HW04_es_next/homework/js/tasks.js
+++ FL12_Webinar_HW04_es_next/homework/js/tasks.js
@@ -46,36 +46,3 @@ async function fetchWithAsyncAwait() {
   names.sort((a, b) => a[0].toLowerCase().charCodeAt(0) - b[0].toLowerCase().charCodeAt(0));
   console.log(names);
 }
-
-//todo: delete lines below
-console.log(maxElement([1, 3, 4, 56, 7, 8, 76, 5, 241, 5, 356, 567, 2])); //?
-
-const array = [1, 2, 3];
-const copiedArray = copyArray(array);
-console.log(array, copiedArray);
-console.log(array === copiedArray);
-
-const firstEnhancedObject = addUniqueId({ name: 123 }); //?
-const secondEnhancedObject = addUniqueId({ name: 123 }); //?
-console.log(firstEnhancedObject === secondEnhancedObject);
-console.log(firstEnhancedObject.id === secondEnhancedObject.id);
-
-const oldObj = {
-  name: "Someone",
-  details: { id: 1, age: 11, university: "UNI" }
-};
-console.log(regroupObject(oldObj));
-
-{
-  const array = [1, 1, 23, 3, 4, 5, 6, 5, 4, 23, 2, 1, 1, 1, 1, 1];
-  console.log(findUniqueElements(array));
-}
-
-const phoneNumber = "0123456789";
-console.log(hideNumber(phoneNumber));
-
-add(1, 3); //?
-// add(1);
-
-fetchWithPromise(); //?
-fetchWithAsyncAwait();
