37808c9b87cfe2c0226da37d4e68210228c578ce
Roman Cherepanov <romach007@gmail.com>
2020-03-03 02:52:52 +0200

    Create simple todo list

--- FL12_Webinar_HW6/task2/js/index.js
+++ FL12_Webinar_HW6/task2/js/index.js
@@ -12,3 +12,40 @@ const todos = [
     done: true
   }
 ];
+
+const removeCallback = function() {
+  const todoIndex = $(".list li").index($(this).parent());
+  todos.splice(todoIndex, 1);
+  $(this)
+    .parent()
+    .remove();
+};
+$(".item-remove").click(removeCallback);
+
+const doneCallback = function() {
+  const todoIndex = $(".list li").index($(this).parent());
+  todos[todoIndex].done = !todos[todoIndex].done;
+  $(this)
+    .parent()
+    .find(".item-text")
+    .toggleClass("done");
+};
+$(".item-text").click(doneCallback);
+
+$add.click(function(event) {
+  event.preventDefault();
+  const newTodo = {
+    text: $input.val(),
+    done: false
+  };
+  todos.push(newTodo);
+  $text = $(`<span class="item-text">${newTodo.text}</span>`);
+  $text.click(doneCallback);
+  $remove = $(`<button class="item-remove">Remove</button>`);
+  $remove.click(removeCallback);
+  $item = $(`<li class="item"></li>`);
+  $item.append($text);
+  $item.append($remove);
+  $list.append($item);
+  $("#add-input").val("");
+});
